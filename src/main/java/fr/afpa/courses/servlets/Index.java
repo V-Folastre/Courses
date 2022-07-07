98a8c147ce6c842b660430e70d9271cbdaf74474
Caliendo Julien <caliendo@hotmail.fr>
2022-07-07 08:25:59 +0200

    Ajout de commentaire sur la Servlet Index

--- src/main/java/fr/afpa/courses/servlets/Index.java
+++ src/main/java/fr/afpa/courses/servlets/Index.java
@@ -7,11 +7,15 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
+// Servlet d'accueil dans web.xml
 @WebServlet("/index")
 public class Index extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // En utilisant la DAL
         ListeSQL listeSQL = new ListeSQL();
+        // Je selectionne toutes les listes de courses en BDD
+        // Et je les envoie a la jsp index.jsp
         request.setAttribute("listes", listeSQL.selectAll());
         request.getRequestDispatcher("WEB-INF/index.jsp").forward(request, response);
     }
